1e0ebf52ba32f2a0eb02446d7c6ce14b6d9782e0
Tarun Saini <tarunsaini2807@gmail.com>
2025-09-14 19:00:06 +0530

    fix: update cookie sameSite policy for production

--- controllers/authController.js
+++ controllers/authController.js
@@ -48,7 +48,7 @@ const loginUser = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV !== 'development', 
-            sameSite: 'strict', 
+            sameSite: process.env.NODE_ENV !== 'development' ? 'none' : 'strict', 
             maxAge: 30 * 24 * 60 * 60 * 1000, 
         });
 
